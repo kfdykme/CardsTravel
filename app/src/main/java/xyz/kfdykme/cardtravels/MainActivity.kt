b6134c60da0b0de831051e1ed55f67a83a2ab82b
kfdykme <wim.k.f@live.com>
2018-03-06 22:32:47 +0800

    update all newly

--- app/src/main/java/xyz/kfdykme/cardtravels/MainActivity.kt
+++ app/src/main/java/xyz/kfdykme/cardtravels/MainActivity.kt
@@ -1,66 +1,145 @@
 package xyz.kfdykme.cardtravels
 
-import android.content.Intent
+import android.content.Context
+import android.content.DialogInterface
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.GridLayoutManager
-import android.view.Menu
-import android.view.MenuItem
-import android.view.View
-import io.realm.Realm
-import io.realm.RealmObject
+import android.view.*
 
 import kotlinx.android.synthetic.main.content_main.*
-import xyz.kfdykme.cardtravels.card.CardEditActivity
+import xyz.kfdykme.cardtravels.card.CardPresenter
+import xyz.kfdykme.cardtravels.card.CardView
 import xyz.kfdykme.cardtravels.data.Card
-import xyz.kfdykme.cardtravels.page.cards.adapter.CardsAdapter
-import xyz.kfdykme.cardtravels.travel.TravelActivity
+import xyz.kfdykme.cardtravels.travel.TravelPresenter
+import xyz.kfdykme.cardtravels.travel.TravelView
+import xyz.kfdykme.cardtravels.util.ActivityUtils
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() ,OnFragmentChangeListener{
 
+
+    val STATE_TRAVEL = 0
+
+    val STATE_CARD= 1
+
+    var context: Context? = null
+
+    var cardFragment:CardView? = null
+
+    var travelFragment : TravelView? = null
+
+    var state:Int = STATE_TRAVEL
+
+    override fun onAdd(type: String) {
+        when(type){
+            "card"->{
+                ActivityUtils.addFragmentToActivity(supportFragmentManager, cardFragment!!,R.id.travelFrame)
+
+            }
+            "travel"->{
+                ActivityUtils.addFragmentToActivity(supportFragmentManager, travelFragment!!,R.id.travelFrame)
+
+            }
+        }
+
+    }
+
+    override fun onReplace(type: String) {
+
+        when(type){
+            "card"->{
+                ActivityUtils.replaceFragmentToActivity(supportFragmentManager,cardFragment!!,R.id.travelFrame)
+                state = STATE_CARD
+
+            }
+            "travel"->{
+                ActivityUtils.replaceFragmentToActivity(supportFragmentManager,travelFragment!!,R.id.travelFrame)
+                state = STATE_TRAVEL
+
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        //test data
-       // val items = ArrayList<String>(listOf("ChengDu","BeiJing","ChengDu","BeiJing","ChengDu","BeiJing","ChengDu","BeiJing","ChengDu","BeiJing","GuangZhou") )
-        val items = Realm.getDefaultInstance().where(Card::class.java).findAll()
-        //init recyclerview
-        rv.adapter = CardsAdapter(this,items,0)
-        rv.layoutManager = GridLayoutManager(this,2)
+        context = this
 
 
-        //init bottom navigation(contains two buttons)
-        btTravel.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(view: View?) {
-                val intent = Intent()
-                intent.setClass(this@MainActivity, TravelActivity::class.java)
-                startActivity(intent)
-            }
-        })
-        btCard.setOnClickListener(object:View.OnClickListener{
-            override fun onClick(p0: View?) {
+        travelFragment = supportFragmentManager.findFragmentById(R.id.travelFrame) as TravelView?
 
-                startActivity(Intent(this@MainActivity, CardEditActivity::class.java))
-            }
-        })
-    }
+        if(travelFragment == null){
+            travelFragment = TravelView.newInstance()
+            onAdd("travel")
+            travelFragment?.setOnOpenCardListener(object :TravelView.OnOpenCardListener{
+                override fun open(card: Card) {
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+                        cardFragment = CardView.newInstance()
+                        cardFragment!!.card = card
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+                        btAdd.visibility = View.GONE
+                        onReplace("card")
+                        var cardPresenter: CardPresenter = CardPresenter(cardFragment!!)
+
+                }
+
+            })
         }
+
+
+        var travelPresenter = TravelPresenter(travelFragment!!)
+
+
+        btAdd.setOnClickListener(object :View.OnClickListener{
+            override fun onClick(v: View?) {
+                cardFragment = CardView.newInstance()
+                onReplace("card")
+                var cardPresenter: CardPresenter = CardPresenter(cardFragment!!)
+                btAdd.visibility = View.GONE
+            }
+        })
     }
+
+    override fun onBackPressed() {
+        when(state){
+            STATE_CARD->{
+                AlertDialog.Builder(this)
+                        .setMessage(getString(R.string.askSaveCardEdit))
+                        .setNegativeButton("No",object: DialogInterface.OnClickListener{
+                            override fun onClick(dialog: DialogInterface?, which: Int) {
+                                onReplace("travel")
+                                btAdd.visibility = View.VISIBLE
+                            }
+                        })
+                        .setPositiveButton("Yes",object : DialogInterface.OnClickListener{
+                            override fun onClick(dialog: DialogInterface?, which: Int) {
+
+                                cardFragment?.doSave()
+                                onReplace("travel")
+                                btAdd.visibility = View.VISIBLE
+                            }
+                        })
+                        .create().show()
+            }
+        }
+
+
+
+    }
+
+    override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
+
+        when(cardFragment?.touchStatus){
+            cardFragment?.NORMAL_TOUCH_STATUS->return super.dispatchTouchEvent(ev)
+            cardFragment?.ALERT_TOUCH_STATUS->{
+                cardFragment?.touchListener?.onTouch(cardFragment?.v,ev)
+                return false
+            }
+        }
+        return false
+    }
+
+
+
 }
